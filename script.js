0adb84bdfbfc999b27605a02b5e7c2a522ff2cc4
Stefan Schlaghuber <stefan.schlaghuber@gmail.com>
2024-01-03 18:29:55 +0100

    points_direction

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const s = 40
 const nx = parseInt(c.width / s)
 const ny = parseInt(c.height / s)
 const arcSize = s*0.65
+const size = 5
 
 const vectors = new Array(nx)
 
@@ -65,37 +66,41 @@ function draw(){
 
     for(let i = 0;i<nx;i+=1){
         for(let j = 0;j<ny;j+=1){
-            let vector = vectors[i][j]
+            /*let vector = vectors[i][j]
             ctx.beginPath();
             ctx.fillStyle = ctx.strokeStyle = `rgb(
                 0,
                 ${Math.floor(255 - (vector.noise() * 255))},
                 ${Math.floor((vector.noise() * 100))})`;
-            ctx.arc(vector.origin.x*s + s/2, vector.origin.y*s + s/2, /*vector.noise() * arcSize*/ 1, 0, 2 * Math.PI);
+            ctx.arc(vector.origin.x*s + s/2, vector.origin.y*s + s/2, /*vector.noise() * arcSize 1, 0, 2 * Math.PI);
             ctx.fill()
             ctx.stroke();
 
-            vector.move()
+            vector.move()*/
 
             /*ctx.beginPath();
+            ctx.fillStyle = ctx.strokeStyle = `rgb(0,0,0)`;
             ctx.fillStyle = ctx.strokeStyle = `rgb(
                 0,
                 ${Math.floor(255 - noise(i,j) * 42.5 * i)},
                 ${Math.floor(255 - noise(i,j) * 42.5 * j)})`;
             ctx.arc(i*s + s/2, j*s + s/2, noise(i,j) * size, 0, 2 * Math.PI);
             ctx.fill()
-            ctx.stroke();*/
+            ctx.stroke();//*/
             
             // calc angle between 0 and 180 deg
-            /*let alpha = noise(i / nScale,j / nScale) * Math.PI * 2 //- Math.PI / 2
+            let alpha = noise(i / nScale,j / nScale) * Math.PI * 2 //- Math.PI / 2
             let a = Math.sin(alpha)*s/2
             let b = Math.cos(alpha)*s/2
 
             ctx.beginPath();
+            ctx.strokeStyle = "rgb(0,0,0)"
+            ctx.arc(i*s + s/2, j*s + s/2, 4, 0, 2 * Math.PI);
+            
+            
             ctx.strokeStyle = "red"
-
             ctx.moveTo(i*s + s/2 - b, j*s + s/2 - a)
-            ctx.lineTo(i*s + s/2 + b, j*s + s/2 + a)*/
+            ctx.lineTo(i*s + s/2 + b, j*s + s/2 + a)//*/
 
 
             //ctx.moveTo(i*s, j*s + s/2)
